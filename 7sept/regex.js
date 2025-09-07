bff45ccbbacc91fea97244d8eed67793768515f1
shaileshsonareg <156632889+shaileshsonareg@users.noreply.github.com>
2025-09-07 11:16:26 +0000

    regex

---
+++ 7sept/regex.js
@@ -0,0 +1,67 @@
+pass_re = /^[A-Za-z]{8, 16}$/
+
+console.log(pass_re.test(""))
+
+
+
+
+
+
+
+
+// girlsname_re = /[aeiou]$/
+
+// console.log(girlsname_re.test("kiran"))
+
+
+
+
+
+
+
+
+// var email_re = /^[a-z0-9$]+\$@[a-z0-9]+\.[a-z]{2,3}$/
+
+// emailid = "shubha$m$@yahoo.com"
+
+// console.log(email_re.test(emailid))
+
+
+
+
+
+
+
+
+// validname_re = /^[ ?a-zA-Z']+$/
+
+// console.log(validname_re.test("Carl & D'Costa"));
+
+
+
+
+
+
+
+
+
+
+
+// pin_re = /^[0-9]{6}$/
+
+// console.log(pin_re.test("123456"));
+
+
+
+
+
+
+
+
+// phone_re = /^[0-9]{8,10}$/
+
+// phonenumber = "987654320"
+// galatphonenumber = "a"
+
+// console.log(phone_re.test(phonenumber));
+// console.log(phone_re.test(galatphonenumber));
