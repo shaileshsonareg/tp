bb2615ff760904e87e8d79d2fa5c3d3d85406b03
shaileshsonareg <156632889+shaileshsonareg@users.noreply.github.com>
2025-09-21 12:31:08 +0000

    array of employees

--- 21sept/json.js
+++ 21sept/json.js
@@ -28,9 +28,9 @@ console.log("Name: ", rec.name);
 
 emps = [
     {name: "Jitesh", age: 18, salary: 21000},
-    {name: "Sneha", age: 21, salary: 22000},
-    {name: "Ani", age: 20, salary: 23000},
-    {name: "Vj", age: 31, salary: 24000},
+    {name: "Sneha", age: 21, salary: 100000},
+    {name: "Ani", age: 20, salary: 100000},
+    {name: "Vj", age: 31, salary: 100000},
 ]
 
 console.log("Anirudha ka salary: ", emps[2].salary);
@@ -42,6 +42,17 @@ for(i = 0; i < emps.length; i++) {
 
 console.log("Total salary: ", total_salary);
 
+max = 0
+for(i = 0; i < emps.length; i++) {
+    if(emps[i].salary > max) {
+        max = emps[i].salary
+    }
+} 
 //CW: WAP to find highest salaried employees from array
+for(i = 0; i < emps.length; i++) {
+    if(emps[i].salary == max) {
+        console.log("Highest: ", emps[i].name);
+    }
+}
 
 
