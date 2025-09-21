b5eb652303e038e070afa6970e511cf8e4cef698
shaileshsonareg <156632889+shaileshsonareg@users.noreply.github.com>
2025-09-21 10:52:10 +0000

    array

---
+++ 21sept/array.js
@@ -0,0 +1,22 @@
+var arr = [1, 2, 4, 6, 7]
+
+console.log(arr);
+console.log("Length: ", arr.length);
+console.log("First: ", arr[0]);
+console.log("Last: ", arr[arr.length - 1]);
+
+for(i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+//output: 
+/**
+arr[0] = 1 
+arr[1] = 2 
+arr[2] = 4 
+arr[4] = 6 
+arr[5] = 7 
+
+ */
+
+
