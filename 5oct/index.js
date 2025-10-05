7e6f5e03aabf2e9343c6b247648808622786e364
shaileshsonareg <156632889+shaileshsonareg@users.noreply.github.com>
2025-10-05 06:39:13 +0000

    if else javascript

---
+++ 5oct/index.js
@@ -0,0 +1,27 @@
+a = 50
+b = 60
+c = 70
+
+if(a > b && a > c) {
+    console.log(`${a} is greatest`);
+} else  if (b > c) {
+    console.log(`${b} is greatest`);
+} else {
+    console.log(`${c} is greatest`);
+}
+
+
+
+
+/*
+x = 0
+
+if(x > 0) {
+    console.log(`${x} is +ve`);
+} else if (x < 0){
+    console.log(`${x} is -ve`);
+} else {
+    console.log(`${x} is zero`);
+}
+
+*/
